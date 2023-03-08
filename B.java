9ab80668836c3b1dd55c514845d6434ebb5b006b
Ashwani Tiwari <tiwariashwani5534@gmail.com>
2023-03-08 22:43:46 +0530

    adding B.java

---
+++ B.java
@@ -0,0 +1,17 @@
+class A{
+    A(){
+        System.out.println("noob");
+    }
+}
+
+class C extends A{
+    C(){
+        System.out.println("dhruv");
+    }
+}
+
+class B{
+    public static void main(String[] args){
+        C x = new C();
+    }
+}
