8a3ca8a5b886bccee0c8384a06e30cd65f966f7a
Ashwani Tiwari <tiwariashwani5534@gmail.com>
2023-03-08 21:29:24 +0530

    initial commit

---
+++ A.java
@@ -0,0 +1,11 @@
+class A{
+    A(){
+        System.out.println("Ash");
+    }
+
+    public static void main(String[] args ){
+        A x  = new A();
+    }
+}
+
+// Constructor can automatically invoked(call).
