a9d3f15c52003745601d4c2661e9eb03ad82d5f9
tachyon333 <tiwariashwani5534@gmail.com>
2023-08-19 09:39:15 +0530

    adding array2.java

---
+++ array2.java
@@ -0,0 +1,18 @@
+class array2 {
+    
+    public static void main(String[] args){
+
+        int[] a = new int[4];
+
+        a[0] = 1;
+        a[1] = 23;
+        a[2] = 43;
+        a[3] = 12;
+
+        for(int i=0 ; i<a.length ; i++){
+            System.out.println(a[i]);
+        }
+    }
+
+        
+}
